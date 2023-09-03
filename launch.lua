e7cc8509ef5442b91144affbf4eb67417cee895f
oeper <82303578+OEPERdev@users.noreply.github.com>
2023-09-03 15:18:36 +0800

    Create launch.lua

---
+++ launch.lua
@@ -0,0 +1,32 @@
+task.spawn(function()
+			errMsg = Instance.new("Hint")
+			errMsg.Parent = game:GetService('CoreGui')
+			errMsg.Text = tostring("Multiple game hub made by oeperdev. NONE OF THESE SCRIPTS ARE MINE")
+			task.wait(10)
+			errMsg:Destroy()
+		end)
+  
+repeat
+	task.wait()
+until game:IsLoaded()
+
+if game.PlaceId ~= 8737602449 and game.PlaceId ~= 8943844393 then --pd
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OEPERdev/pdautofarm/main/autofarm",true))() 
+  
+else if game.PlaceId ~= 1962086868 then --toh
+    loadstring(game:HttpGet(('https://pastebin.com/raw/0n2XACgm'),true))()
+    
+  else if game.PlaceId ~= 1537690962 then --bss
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/BadScripters/CloudHub/main/hub%22))()
+          
+            else if game.PlaceId ~= 263761432 then --horhous
+           loadstring(game:HttpGet("https://gist.githubusercontent.com/RavyarTheBest/b0f2640392a6c107f2dc159d00f3ab40/raw/f0fc6c0906ee035edae1bb5248d1b2f6c20908da/horrific", true))()
+
+             else if game.PlaceId ~= 155615604 then --prilife
+              loadstring(game:HttpGet("https://raw.githubusercontent.com/GwnStefano/NexusHub/main/Main", true))()
+              
+              else if game.PlaceId ~= 6872265039 then --gaybedwars
+                              loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+                
+                              else if game.PlaceId ~= 537413528 then --babft
+                  loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/littlegui/main/Build-A-Boat'))()
