8c62a4265fb2d471f555c59d175509b53309f9b7
oeper <82303578+OEPERdev@users.noreply.github.com>
2023-09-03 17:03:54 +0800

    Update launch.lua

--- launch.lua
+++ launch.lua
@@ -13,23 +13,23 @@ until game:IsLoaded()
 if game.PlaceId = 8737602449 and game.PlaceId = 8943844393 then --pd
   loadstring(game:HttpGet("https://raw.githubusercontent.com/OEPERdev/pdautofarm/main/autofarm",true))() 
   
- if game.PlaceId = 1962086868 then --toh
+else if game.PlaceId = 1962086868 then --toh
     loadstring(game:HttpGet(('https://pastebin.com/raw/0n2XACgm'),true))()
     
-   if game.PlaceId = 1537690962 then --bss
+else if game.PlaceId = 1537690962 then --bss
     loadstring(game:HttpGet("https://raw.githubusercontent.com/BadScripters/CloudHub/main/hub"))()
           
-             if game.PlaceId = 263761432 then --horhous
+	else if game.PlaceId = 263761432 then --horhous
            loadstring(game:HttpGet("https://gist.githubusercontent.com/RavyarTheBest/b0f2640392a6c107f2dc159d00f3ab40/raw/f0fc6c0906ee035edae1bb5248d1b2f6c20908da/horrific", true))()
 
-              if game.PlaceId = 155615604 then --prilife
+		else if game.PlaceId = 155615604 then --prilife
               loadstring(game:HttpGet("https://raw.githubusercontent.com/GwnStefano/NexusHub/main/Main", true))()
               
-               if game.PlaceId = 6872265039 then --gaybedwars
+			else if game.PlaceId = 6872265039 then --gaybedwars
                               loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
                 
-                               if game.PlaceId = 537413528 then --babft
+				else if game.PlaceId = 537413528 then --babft
                   loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/littlegui/main/Build-A-Boat'))()
 
-							  if game.PlaceId = 142823291 then --mm2
+					else if game.PlaceId = 142823291 then --mm2
 								loadstring(game:GetObjects("rbxassetid://4001118261")[1].Source)()
